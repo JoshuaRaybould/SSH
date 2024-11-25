48375554cd559fd99e885ecd7912aa32dd19aaf9
jxb1498 <jxb1498@student.bham.ac.uk>
2024-11-25 18:42:18 +0000

    Add basic code for javafx

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -3,12 +3,22 @@
  */
 package org.example;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Hello, JavaFX!");
+        Scene scene = new Scene(label, 400, 300);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("JavaFX Example");
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        launch(args);
     }
 }
