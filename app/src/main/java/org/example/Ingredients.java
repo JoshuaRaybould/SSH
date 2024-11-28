49cab929ff9c74901cf3f43cf64711cef5eb924e
varsityyy <topchainproductions@gmail.com>
2024-11-28 19:01:22 +0000

    Update from days to minutes
    Adjusted to work in minutes rather than days.

--- app/src/main/java/org/example/Ingredients.java
+++ app/src/main/java/org/example/Ingredients.java
@@ -1,24 +1,26 @@
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
 public class Ingredients {
     private String name;
-    private LocalDate capturedDate;
+    private LocalDateTime capturedDate;
     private int estimatedShelfLife;
 
 
     public Ingredients(String name, LocalDate capturedDate, int estimatedShelfLife){
         this.name = name;
-        this.capturedDate = capturedDate;
+        this.capturedDate = capturedDate.atStartOfDay();
         this.estimatedShelfLife = estimatedShelfLife;
     }
 
     public double calculateQuality() {
         long daysSinceCaptured = ChronoUnit.MINUTES.between(capturedDate, LocalDate.now()); //in reality this wouldbe days, but for the sake of testing, we will use minutes
-        if (daysSinceCaptured > estimatedShelfLife) {
+        
+        if (daysSinceCaptured > estimatedShelfLife * 1440) {
             return 0;
         }
-        return Math.max(0, 1 - ((double) daysSinceCaptured / (estimatedShelfLife + 1)));
+        return Math.max(0, 1 - ((double) daysSinceCaptured / (estimatedShelfLife + 1) * 1440));
     }
 
     public String getName() {
