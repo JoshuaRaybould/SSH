6f17f84baf3708e3065c6c02c96604091048fcad
varsityyy <topchainproductions@gmail.com>
2024-12-03 17:07:11 +0000

    Add test for a loaded recipe
    Created a test for the data types in a recipe

---
+++ app/src/test/java/org/example/LoadRecipeTest.java
@@ -0,0 +1,28 @@
+package org.example;
+
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class LoadRecipeTest {
+
+    @Test
+    public void testDataTypes(){
+        LoadRecipes loadRecipes = new LoadRecipes();
+
+        Recipe[] recipes = loadRecipes.ReturnRecipes();
+
+        Recipe recipe1 = recipes[0];
+
+        assertTrue(recipe1.getName() instanceof String, "Name should be a String");
+        
+      
+        assertTrue(recipe1.getInstructions() instanceof String, "Instructions should be a String");
+
+       
+        assertTrue(recipe1.getIngredients() instanceof String[], "Ingredients should be a String array");
+
+        
+        assertTrue(recipe1.getQuantities() instanceof int[], "Quantities should be an int array");
+    }
+}
