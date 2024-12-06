d2636c578cae83943a85679c97cc7372dafc67ab
JoshuaRaybould <138737258+JoshuaRaybould@users.noreply.github.com>
2024-12-06 00:28:23 +0000

    Delete things that are no longer necessary in db
    removed the recipes since these aren't handled here, along with the
    tables we originally had to support this. Renamed folder for clarity
    that this is for the SSH's own database.

--- SSH_cloud_database/databaseSSH.sql
+++ SSH_cloud_database/databaseSSH.sql
@@ -9,6 +9,7 @@ CREATE TABLE fridge_items (
     fridge_item_name varchar(255) NOT NULL,
     fridge_item_price money NOT NULL,
     estimated_shelf_life integer NOT NULL,
+    item_type varchar(64)
     PRIMARY KEY(fridge_item_id)
 );
 
@@ -16,26 +17,11 @@ CREATE TABLE tenants_fridge_items (
     tenant_id integer NOT NULL references tenants(tenant_id),
     fridge_item_id integer NOT NULL references fridge_items(fridge_item_id),
     quantity integer NOT NULL,
-    date_time timestamp NOT NULL, -- In reality we'd only need DATE here, but we might want to simulate a daily job happening every minute so we can see it works
+    date_time timestamp NOT NULL,
     quality decimal NOT NULL,
     PRIMARY KEY (tenant_id, fridge_item_id)
 );
 
-CREATE TABLE recipes (
-    recipe_id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
-    recipe_name varchar(255) NOT NULL,
-    recipe_instructions TEXT NOT NULL,
-    PRIMARY KEY (recipe_id)
-);
-
-CREATE TABLE recipes_fridge_items (
-    recipe_id integer NOT NULL references recipes(recipe_id),
-    fridge_item_id integer NOT NULL references fridge_items(fridge_item_id),
-    required_quantity integer NOT NULL,
-    PRIMARY KEY (recipe_id, fridge_item_id)
-);
-
--- INSERT INTO tenants (tenant_id, tenant_name) VALUES (1, 'Jamal');
 
 COPY fridge_items
 FROM '/docker-entrypoint-initdb.d/ingredients.csv'
