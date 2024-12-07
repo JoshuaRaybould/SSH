681928ca4ca7abad95fbca74e0c804366b8d7e92
JoshuaRaybould <138737258+JoshuaRaybould@users.noreply.github.com>
2024-12-07 01:26:57 +0000

    Remove old ingredients test

--- app/src/main/java/org/example/MemoryIngredients.java
+++ app/src/main/java/org/example/MemoryIngredients.java
@@ -1,40 +1,21 @@
 package org.example;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class MemoryIngredients implements IIngredientsOutput{
-    private String tenantString = "";
-    private List<String> ingedientStrings; // This is for use with test user
+    private String tenantName = "";
     private List<Ingredient> ingredients;
 
     @Override
     public void reportIngredients(int tenantId) {
         Tenant tenant = new Tenant(tenantId);
-        String tenantName = tenant.getTenantName();
+        tenantName = tenant.getTenantName();
         TenantIngredients tenantIngredients = new TenantIngredients(tenantId);
         ingredients = tenantIngredients.getIngredients();
-
-        if (tenantId == 1) { // If we are the test user
-            if (!tenantName.equals("")) {
-                tenantString = "Tenant with id " + tenantId + ", " + tenantName + ", has the following ingedients";
-                for (Ingredient ingredient: ingredients) {
-                    ingedientStrings.add("Name:" + ingredient.getName() + ", Quantity:" + ingredient.getQuantity() + ", Quality:" + ingredient.getQuality());
-                }
-            }
-        } 
     }
 
-    public MemoryIngredients() {
-        ingedientStrings = new ArrayList<>();
-    }
-
-    public String getTenantString(){
-        return tenantString;
-    }
-
-    public List<String> getIngredientStrings() {
-        return ingedientStrings;
+    public String getTenantName(){
+        return tenantName;
     }
 
     public List<Ingredient> getIngredients() {
