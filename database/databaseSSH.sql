507e0de1ba40fd72feaf167f62a0fe098a9ab1ba
ArmanAli05 <akhterali129129@gmail.com>
2024-12-07 21:42:09 +0000

    ?

--- database/databaseSSH.sql
+++ database/databaseSSH.sql
@@ -17,7 +17,7 @@ CREATE TABLE tenants_fridge_items (
     tenant_id integer NOT NULL references tenants(tenant_id),
     fridge_item_id integer NOT NULL references fridge_items(fridge_item_id),
     quantity integer NOT NULL,
-    date_time timestamp NOT NULL,
+    date_time timestamp NOT NULL, -- In reality we'd only need DATE here, but we might want to simulate a daily job happening every minute so we can see it works
     quality decimal NOT NULL,
     PRIMARY KEY (tenant_id, fridge_item_id)
 );
@@ -36,22 +36,25 @@ CREATE TABLE recipes_fridge_items (
     PRIMARY KEY (recipe_id, fridge_item_id)
 );
 
-INSERT INTO tenants (tenant_name)
-SELECT 'Test User'
-WHERE NOT EXISTS (SELECT 1 FROM tenants WHERE tenant_name = 'Test User');
+-- INSERT INTO tenants (tenant_id, tenant_name) VALUES (1, 'Jamal');
 
 COPY fridge_items
 FROM '/docker-entrypoint-initdb.d/ingredients.csv'
-DELIMITER ',' CSV HEADER;
+DELIMITER ','
+CSV HEADER;
 
 COPY recipes
 FROM '/docker-entrypoint-initdb.d/recipes.csv'
-DELIMITER ',' CSV HEADER;
+DELIMITER ','
+CSV HEADER;
 
 COPY tenants
 FROM '/docker-entrypoint-initdb.d/tenants.csv'
-DELIMITER ',' CSV HEADER;
+DELIMITER ','
+CSV HEADER;
 
 COPY tenants_fridge_items
 FROM '/docker-entrypoint-initdb.d/tenants_fridge_items.csv'
-DELIMITER ',' CSV HEADER;
+DELIMITER ','
+CSV HEADER;
+
