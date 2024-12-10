db4b7202537fafdfe5ad927b20356a237c4aba42
JoshuaRaybould <138737258+JoshuaRaybould@users.noreply.github.com>
2024-12-10 15:38:17 +0000

    Remove database tables we don't need
    Also changed date time to use DATE rather than TIMESTAMP since we are
    just using days.

--- database/databaseSSH.sql
+++ database/databaseSSH.sql
@@ -17,34 +17,15 @@ CREATE TABLE tenants_fridge_items (
     tenant_id integer NOT NULL references tenants(tenant_id),
     fridge_item_id integer NOT NULL references fridge_items(fridge_item_id),
     quantity integer NOT NULL,
-    date_time timestamp NOT NULL, -- In reality we'd only need DATE here, but we might want to simulate a daily job happening every minute so we can see it works
+    date_time DATE NOT NULL,
     quality decimal NOT NULL,
     PRIMARY KEY (tenant_id, fridge_item_id)
-);
-
-CREATE TABLE recipes (
-    recipe_id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
-    recipe_name varchar(255) NOT NULL,
-    recipe_instructions TEXT NOT NULL,
-    PRIMARY KEY (recipe_id)
-);
-
-CREATE TABLE recipes_fridge_items (
-    recipe_id integer NOT NULL references recipes(recipe_id),
-    fridge_item_id integer NOT NULL references fridge_items(fridge_item_id),
-    required_quantity integer NOT NULL,
-    PRIMARY KEY (recipe_id, fridge_item_id)
 );
 
 -- INSERT INTO tenants (tenant_id, tenant_name) VALUES (1, 'Jamal');
 
 COPY fridge_items
 FROM '/docker-entrypoint-initdb.d/ingredients.csv'
-DELIMITER ','
-CSV HEADER;
-
-COPY recipes
-FROM '/docker-entrypoint-initdb.d/recipes.csv'
 DELIMITER ','
 CSV HEADER;
 
