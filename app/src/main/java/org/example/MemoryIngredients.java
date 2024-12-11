89d7d6dad834643ddb7f3038c00f038d8f25c776
JoshuaRaybould <138737258+JoshuaRaybould@users.noreply.github.com>
2024-12-11 12:37:39 +0000

    Update to generate users for the test

--- app/src/main/java/org/example/MemoryIngredients.java
+++ app/src/main/java/org/example/MemoryIngredients.java
@@ -1,8 +1,9 @@
 package org.example;
 
 import java.util.List;
-
-public class MemoryIngredients implements IIngredientsOutput{
+// This is just to get ingredients into a list for testing
+// It also creates the user if they don't already exist
+public class MemoryIngredients implements IIngredientsOutput{ 
     private String tenantName = "";
     private List<Ingredient> ingredients;
 
@@ -10,8 +11,17 @@ public class MemoryIngredients implements IIngredientsOutput{
     public void reportIngredients(int tenantId) {
         Tenant tenant = new Tenant(tenantId);
         tenantName = tenant.getTenantName();
+        // If the tenant doesn't already exist make it 
+        if (tenantName.equals("")) { // Since this is only used for testing it will only ever create users 1 to 4
+            UserCreation.createUser("User:" + tenantId);
+            tenantName = tenant.getTenantName();
+        }
         TenantIngredients tenantIngredients = new TenantIngredients(tenantId);
         ingredients = tenantIngredients.getIngredients();
+    }
+
+    public Boolean hasSucceeded(){
+        return (!tenantName.equals(""));
     }
 
     public String getTenantName(){
--- app/src/test/java/org/example/TenantIngredientsTest.java
+++ app/src/test/java/org/example/TenantIngredientsTest.java
@@ -1,22 +1,22 @@
 package org.example;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
-public class TenantIngredientsTest { // These tests rely on the database and so I'm not sure they class as unit tests exactly but are still useful checks to have
+public class TenantIngredientsTest { // These tests rely on the database, they are still useful checks to have
+
+    public MemoryIngredients output = new MemoryIngredients();
 
     @Test
-    public void sanityCheckGeneartedUser(){ // This will test a couple users provided they exist (for now this will pass but do nothing meaningful)
-        int[] tenantIds = new int[]{2, 3, 4};
+    public void sanityCheckGeneartedUser(){ // This will test a couple users provided they exist
+        int[] tenantIds = new int[]{1, 2, 3, 4};
 
         for (int tenantId : tenantIds) {
-            MemoryIngredients output = new MemoryIngredients();
             RecipesApp.printTenantIngredients(output, tenantId);
-            if (output.getTenantName().equals("")) break; // The tenant doesn't exist, and so no others can exist after this
+            if (!output.hasSucceeded()) break; // The tenant doesn't exist, and so no others can exist after this
             List<Ingredient> ingredients = output.getIngredients();
 
             assertTrue(ingredients.size() > 0); // All generated users in the database should have at least a few ingredients
@@ -24,24 +24,13 @@ public class TenantIngredientsTest { // These tests rely on the database and so 
                 // Quality should be a value between 0 and 1
                 assertTrue(ingredient.getQuality() >= 0.0 && ingredient.getQuality() <= 1.0); 
 
+                // Quantity should be between 0 and 1000 at most
+                assertTrue(ingredient.getQuantity() >= 0 && ingredient.getQuantity() <= 1000);
+
                 // We may add a more in depth check later, but this would require a new ingredients class with all the ingredients and their types so we know what a reasonable quantity is.              
                 // idea for one more statement, assert true that we find the ingredient in the list of all ingredients (unsure if this is relevant at all)
             }
         }
-
-    }
-
-    @Test
-    public void testNonExistantUser(){
-        int tenantId = 1000; // The id of a nonexistant user
-        MemoryIngredients output = new MemoryIngredients();
-        RecipesApp.printTenantIngredients(output, tenantId);
-
-        List<Ingredient> ingedients = output.getIngredients();
-        
-        // Check we fail appropriately
-        assertEquals("", output.getTenantName());
-        assertEquals(0, ingedients.size());
 
     }
 
